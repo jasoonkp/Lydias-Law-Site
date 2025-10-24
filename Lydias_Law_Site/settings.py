35e32fbe672a6390ade12865c2c2555f865b778e
jasoonkp <jasonprakash1214@gmail.com>
2025-10-24 00:05:45 -0700

    created notifications table, added admin access to user table

--- Lydias_Law_Site/settings.py
+++ Lydias_Law_Site/settings.py
@@ -23,7 +23,7 @@ environ.Env.read_env(os.path.join(BASE_DIR,".env"))
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6+b2mt+l^e3p7cu!yxc%tdos4i+c#gh1^=k=d!a=#4+6)23eg'
+#SECRET_KEY = 'django-insecure-g6+b2mt+l^e3p7cu!yxc%tdos4i+c#gh1^=k=d!a=#4+6)23eg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'Lydias_Law_Site.wsgi.application'
 # Uncomment below for MySQL/PostgreSQL in production:
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
+        default=os.getenv("DATABASE_URL"),
         engine="django.db.backends.mysql",
         conn_max_age=600,
     )
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
+# users/admin.py
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("user_id", "first_name","last_name", "email", "created_at")  # adjust to your fields
+    list_filter = ("created_at",)
+    search_fields = ("last_name", "email")
+    ordering = ("created_at",)
