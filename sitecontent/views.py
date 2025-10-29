ed55716db6ecfc0dd367205bc43ddea59a3f5739
nayelifv <floresvaldeznayeli@gmail.com>
2025-10-28 21:11:07 -0700

    LLW-114 Create Contact Information Section (#21)
    Created the contact information section of the Contact page for both logged in users and non-logged in users. Fixed some issues regarding the styles.css file.

--- sitecontent/views.py
+++ sitecontent/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import WebsiteContent
+from django.contrib.auth import get_user_model
 from django.http import HttpResponseServerError
 
 # Create your views here.
@@ -29,20 +30,34 @@ def home(request):
 
 
 # About Page Request
-def about(request):
+def about(request, client=False):
     try:
-        # Attempt to get the latest content
+        # Attempt to get the latest content from the WebsiteContent table
         content = WebsiteContent.objects.order_by('-versionNumber').first()
 
+        # Fallback if no content exists
         if not content:
-            # Fallback if no content exists
+            
             content = WebsiteContent(
-                nameTitle="No Content Available",
-                aboutMeDescription="No description available."
+                nameTitle = "No Content Available",
+                aboutMeDescription = "No description available."
             )
+        
+        # Fallback if no title exists
+        if content and not content.nameTitle:
+            content.nameTitle = "No Content Availabe"
 
-        # Render the normal about page
-        return render(request, 'about.html', {'content': content})
+        # Fallback if no phone number exists
+        if content and not content.aboutMeDescription:
+            content.aboutMeDescription = "No Content Available"
+
+        # Check which page to render
+        page = 'about.html'
+        if client:
+            page = 'client/about.html'
+
+        # Render the about page
+        return render(request, page, {'content': content})
 
     except Exception as e:
         # Log the error
@@ -50,7 +65,49 @@ def about(request):
 
         # Error page
         return HttpResponseServerError("An error occurred while loading the About page.")
+    
 
+# Contact Page request
+def contact(request, client=False):
+    try:
+        User = get_user_model()
 
+        # Attempt to get the latest content from WebsiteContent table
+        location = WebsiteContent.objects.order_by('-versionNumber').first()
+
+        # Attempts to get Lydia's information from User table
+        lydia = User.objects.filter(role=User.Role.ADMIN).first()
+
+        # Fallback if no location exists
+        if not location:
+            location = WebsiteContent(
+                officeLocation="No Content Available"
+            )
+
+        # Fallback if no email exists
+        if lydia and not lydia.email:
+            lydia.email = "No Content Available"
+
+        # Fallback if no phone number exists
+        if lydia and not lydia.phone_number:
+            lydia.phone_number = "No Content Available"
+
+        # Check which page to render
+        page = 'contact.html'
+        if client:
+            page = 'client/contact.html'
+
+        # Render contact page
+        return render(request, page, {
+            'location': location,
+            'lydia': lydia
+        })
+    
+    except Exception as e:
+        # Log the error
+        print(f"ERROR in about view: {e}")
+
+        # Error page
+        return HttpResponseServerError("An error occurred while loading the Contact page.")
 
 
