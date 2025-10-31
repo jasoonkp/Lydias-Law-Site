d49ebbeefa838d740a725ee79b430e8e40c7e412
reggiee76 <90946376+reggiee76@users.noreply.github.com>
2025-10-30 20:22:05 -0700

    LLW - #196 Create Invoice Table + updated models.py & admin.py (#25)

--- finances/admin.py
+++ finances/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Payment
+from .models import Invoice
 
 # Register your models here.
+
+# Payment Table
+admin.site.register(Payment)
+
+# Invoice Table
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "amount", "paid", "created_at")
+    list_filter = ("paid", "created_at")
+    search_fields = ("email", "first_name", "last_name")
--- finances/models.py
+++ finances/models.py
@@ -16,3 +16,24 @@ class Payment(models.Model):
 
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.PENDING)
     paid_at = models.DateTimeField(null=True, blank=True)
+
+# Invoice Table
+class Invoice(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(    # Foreign key to user who invoice belongs to
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name="invoices",
+        db_index=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    amount = models.PositiveIntegerField(help_text="minor units (cents)")
+    paid = models.BooleanField(default=False)    
+
+    # Output Readability
+    def __str__(self):
+        return f"Invoice #{self.id} - User {self.user.id} - ${self.amount}"
+    
+    class Meta:
+        db_table = "invoices"
+        ordering = ["-created_at"]
