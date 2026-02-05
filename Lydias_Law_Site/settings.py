4500493a38f22761b0d8524e480edf5be8818cd5
nayelifv <floresvaldeznayeli@gmail.com>
2026-02-04 16:24:55 -0800

    LLW-226, 229, 234 Sign In with Google (#54)
    * LLW-226 Set up allauth for Sign In with Google
    
    * LLW-229 Django Socail Account and Requirements
    
    * LLW-234 Wire the frontend button to allauth’s Google Signup

--- Lydias_Law_Site/settings.py
+++ Lydias_Law_Site/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     "crispy_forms",
     "crispy_bootstrap5",
     "core",
@@ -105,7 +107,7 @@ TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates", BASE_DIR / "users" / "templates"
 
 # Base Login/Logout Redirects (keeping these for safety)
 LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "client/dashboard"
+LOGIN_REDIRECT_URL = "/client/dashboard/"
 LOGOUT_REDIRECT_URL = "home"
 
 # Auth Login/Logout Redirects
@@ -122,7 +124,7 @@ AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
 
     # django-allauth for Google login
-    # "allauth.account.auth_backends.AuthenticationBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 # Email Verification Settings
@@ -144,6 +146,16 @@ ACCOUNT_SIGNUP_FIELDS = [
     "email*", "password1*", "password2*",        # required fields
     "first-name", "last-name", "phone-number"  # optional fields
 ]
+
+# Google login requirements
+SOCIALACCOUNT_AUTO_SIGNUP = True
+SOCIALACCOUNT_LOGIN_ON_GET = True
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_FORMS = {
+    "signup": "users.forms.CustomSignupForm"
+}
 
 # Email Verification Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
