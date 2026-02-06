1b71b1993dc22a18d93ecfefe758d1c75bb46fc1
reggiee76 <90946376+reggiee76@users.noreply.github.com>
2026-02-05 17:40:11 -0800

    LLW-177: Install Django-ckeditor

--- Lydias_Law_Site/urls.py
+++ Lydias_Law_Site/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +25,5 @@ urlpatterns = [
     path("", include("core.urls")),
     path("accounts/", include("allauth.urls")),
     path("appointments/", include("appointments.urls")),
-]
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sitecontent/models.py
+++ sitecontent/models.py
@@ -1,21 +1,22 @@
 from django.db import models
+from django_ckeditor_5.fields import CKEditor5Field
 
 class WebsiteContent(models.Model):
     versionNumber = models.AutoField(primary_key=True)
     frontPageHeader = models.CharField(max_length=140, blank=True)
-    frontPageDescription = models.TextField(blank=True)
+    frontPageDescription = CKEditor5Field(blank=True, config_name='default')
 
-    stepParentAdoptionDescription = models.TextField(blank=True)
-    adultAdoptionDescription = models.TextField(blank=True)
-    guardianshipDescription = models.TextField(blank=True)
-    guardianshipToAdoptionDescription = models.TextField(blank=True)
-    independentAdoptionDescription = models.TextField(blank=True)
+    stepParentAdoptionDescription = CKEditor5Field(blank=True, config_name='default')
+    adultAdoptionDescription = CKEditor5Field(blank=True, config_name='default')
+    guardianshipDescription = CKEditor5Field(blank=True, config_name='default')
+    guardianshipToAdoptionDescription = CKEditor5Field(blank=True, config_name='default')
+    independentAdoptionDescription = CKEditor5Field(blank=True, config_name='default')
 
     nameTitle = models.CharField(max_length=120, blank=True)
-    aboutMeDescription = models.TextField(blank=True) 
+    aboutMeDescription = CKEditor5Field(blank=True, config_name='default') 
     officeLocation = models.CharField(max_length=200, blank=True)
 
-    footerDescription = models.TextField(blank=True)
+    footerDescription = CKEditor5Field(blank=True, config_name='default')
 
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(auto_now=True)
