19f5ec123aeb3c1beff99577d399d0adf9d99b9a
MADIL99 <mariaadil98@gmail.com>
2026-02-15 19:48:01 -0800

    LLW-89 Guest Payment Success Page

--- core/views.py
+++ core/views.py
@@ -37,6 +37,7 @@ def payment(r): return render(r, "payment.html")
 def schedule(r): return render(r, "schedule.html")
 def privacy(r): return render(r, "privacy.html")
 def appointment_confirmation(r): return render (r, "appointment_confirmation.html")
+def payment_success(r): return render (r, "payment_success.html")
 
 def login(r): 
     role = r.GET.get("role", "guest")
